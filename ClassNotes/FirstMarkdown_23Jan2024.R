204d83045df6704d6da5f138e8107f6a2baec886
Maryam NouriAiin <maryam.nouri-aiin@uvm.edum>
2024-01-31 17:23:20 -0500

    adding class notes

---
+++ ClassNotes/FirstMarkdown_23Jan2024.R
@@ -0,0 +1,21 @@
+## ----setup, include=FALSE------------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ------------------------------------------------------------------------------------------------
+# First chunk of code in rmd file
+print(pi)
+pi+3
+
+
+## ----eval=TRUE, echo=TRUE------------------------------------------------------------------------
+# Use comments extensively in ALL of your coding!
+Pred <- seq(1,10)     # make a vector of integers from 1 to 10
+Res <- runif(10)      # generate 10 draws from a random uniform (0,1) distribution
+
+# print the random numbers
+print(Res)
+
+# plot the graph
+plot(x=Pred,y=Res,type="b")
+
---
+++ ClassNotes/FirstRScript_25Jan2024.R
@@ -0,0 +1,53 @@
+# First comment to explain what this program is doing.
+# Be expansive and describe it in great detail. This may seem trivial, but will become increasingly important as you create complex programs.
+# Simple script to examine the distribution of the product of two uniform variables
+# Make sure it is readable. Use complete sentences, not cryptic phrases.
+# 6 September 2018
+# NJG
+
+# Preliminaries
+library(ggplot2)
+set.seed(100) #to ensure reproducibility of data so everyone using the same seed get the same random number e.g. the following whoever sets seed at 100 gets the same randome 4 numbers:
+#runif(4)
+#[1] 0.2495337 0.5074827 0.5284712 0.5035891
+library(TeachingDemos) # use this to set the random number seed from a character string
+char2seed("green tea")
+char2seed("green tea",set=FALSE)
+# char2seeds generates random numbers
+
+
+# Global variables
+nRep <- 10000
+
+# Create or read in data
+ranVar1 <- rnorm(nRep)
+# print(ranVar1)
+head(ranVar1)
+
+tail(ranVar1)
+ranVar2 <- rnorm(nRep)
+
+# visualize data
+qplot(x=ranVar1)
+# create product vector
+ranProd <- ranVar1*ranVar2
+length(ranProd)
+
+str(ranProd)
+head(ranProd)
+
+
+########30Jan2024
+# z <- 3+4
+# z
+
+z <- 3+4
+z
+print(z)
+
+# Here is a new comment (not R code!)
+
+
+
+library(knitr)
+purl("FileName.Rmd")
