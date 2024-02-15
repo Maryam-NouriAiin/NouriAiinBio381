b97856e1bd30255380e617c5035e9c262387ea08
Maryam NouriAiin <maryam.nouri-aiin@uvm.edum>
2024-02-14 22:29:33 -0500

    add assignments 4 & 5

---
+++ ClassNotes/MetricsListDataFrames_8Feb2024.R
@@ -0,0 +1,123 @@
+# Matrices, Lists, & Data Frames
+# 8 Feb 2024
+
+
+# a matrix is an atomic vector that is organized into rows and columns
+my_vec <- 1:12
+m <- matrix(data=my_vec,nrow=4)
+print(m)
+m <- matrix(data=my_vec,ncol=3)
+print(m)
+m <- matrix(data=my_vec,ncol=3,byrow=TRUE)
+print(m)
+
+my_m_data<- c(1,2,3,
+              4,5,6,
+              7,8,9)
+my_new_matrix <- matrix(data=my_m_data, nrow=3, byrow=TRUE)
+print(my_new_matrix)
+
+
+# Lists are atomic vectors but each element
+# can hold things of different types and different sizes
+myList <- list(1:10, matrix(1:8,nrow=4,byrow=TRUE), letters[1:3],pi)
+str(myList)
+print(myList)
+# using [] gives you a single item, which is of type list
+myList[4]
+myList[4] - 3 # no, can't subtract a number from a list!
+# single brackets gives you only the element in that slot, which is always of type list
+# to grab the object itself, use [[]]
+myList[[4]]
+myList[[4]] - 3 # now we have the contents
+# if a list has 10 elements it is like a train with 10 cars
+# [[5]] gives you the contents of car #5, create a train with just car 5
+##[c(4,5,6)] gives you a little train with cars 4, 5, and 6
+# once the double bracket is called, you can access individual elements as before
+myList[[2]]
+myList[[2]][4,1]
+# name list items when they are created
+myList2 <- list(Tester=FALSE,littleM=matrix(1:9,nrow=3))
+print(myList2)
+
+# named elements can be accessed with dollar sign
+myList2$littleM[2,3] # get row 2, column3
+myList2$littleM # show whole matrix
+myList2$littleM[2,] # show second row, all columns
+myList2$littleM[2] # what does this give you?
+# The "unlist" strings everything back into
+# a single atomic vector; coercion is used if there are mixed data types
+unRolled <- unlist(myList2)
+print(unRolled)
+# The most common use of list: output from a linear model is a list
+library(ggplot2)
+yVar <- runif(10)
+xVar <- runif(10)
+
+
+myModel <- lm(yVar~xVar)
+qplot(x=xVar,y=yVar)
+# look at output in myModel
+print(myModel)
+# full results are in summary
+print(summary(myModel))
+# drill down into summary to get what we want
+str(summary(myModel))
+summary(myModel)$coefficients
+summary(myModel)$coefficients["xVar","Pr(>|t|)"]
+summary(myModel)$coefficients[2,4]
+# use unlist instead
+u <- unlist(summary(myModel))
+print(u)
+mySlope <- u$coefficients2
+myPval <- u$coefficients8
+
+
+# a data frame is a list of equal-lengthed vectors, each of which is a column
+varA <- 1:12
+varB <- rep(c("Con","LowN","HighN"),each=4)
+varC <- runif(12)
+dFrame <- data.frame(varA,varB,varC,stringsAsFactors=FALSE)
+print(dFrame)
+str(dFrame)
+# add another row with rbind
+# make sure you add a list, with each item corresponding to a column
+# newData <- data.frame(list(varA=13,varB="HighN",varC=0.668),stringsAsFactors=FALSE)
+newData <- list(varA=13,varB="HighN",varC=0.668)
+print(newData)
+str(newData)
+# now bind them
+dFrame <- rbind(dFrame,newData)
+str(dFrame)
+tail(dFrame)
+
+# adding another column is a little easier
+#newVar <- data.frame(varD=runif(13))
+newVar <- runif(13)
+dFrame <- cbind(dFrame,newVar)
+head(dFrame)
+
+
+# create a matrix and data frame with same structures 
+zMat <- matrix(data=1:30,ncol=3,byrow=TRUE) 
+zDframe <- as.data.frame(zMat) # coerce it
+str(zMat) # an atomic vector with 2 dimensions 
+str(zDframe) # note horizontal layout of variabes!
+head(zDframe) # note automatic variable names 
+head(zMat) # note identical layout
+# element referencing is the same in both 
+zMat[3,3]
+zDframe[3,3]
+# so is column referencing
+zMat[,3]
+zDframe[,3]
+zDframe$V3 # note use of $ and named variable column
+# and row referencing
+zMat[3,]
+zDframe[3,] # note variable names and row number shown
+# what happens if we reference only one dimension?
+zMat[2] # takes the second element of atomic vector (column fill)
+zDframe[2] # takes second atomic vector (= column) from list
+zDframe["V2"]
+zDframe$V2
+
---
+++ README.md
@@ -0,0 +1,25 @@
+# Computational Biology
+
+This is where I'll be posting my homework assignments and projects for the course. I'm excited to use this platform to showcase my progress, collaborate with classmates, and dive into the fascinating world of computational biology. 
+
+
+<div style="background-color: #a2ac94 ; color: #000; padding: 10px; border-radius: 5px;">
+  <p> Assignment </p> </div>
+
+
+- [Assignment #2](homework-2.html) January 24, 2024
+- [Assignment #3](homework-3.html) January 31, 2024
+- [Assignment #4](homework-4.html) February 7, 2024
+- [Assignment #5](homework-5.html) February 14, 2024
+
+
+<div style="background-color:#cbccbc; color: #000; padding: 10px; border-radius: 5px;">
+  <p> Class notes </p> </div>
+  
+- [Markdown](FirstMarkdown_23Jan2024.html) January 23, 2024
+- [R script](FirstRScript_25Jan2024.R) January 25, 2024
+- [Regular Expressions](RegularExperssionScratchFile_30Jan2024) January 30, 2024
+- [Atomic Vector l](AtomicVectorI_01Feb2024.R) February 1, 2024
+- [Atomic Vector ll](AttomicVectorII_6Feb2024.R) February 6, 2024
+- [Metrics List Data Frames](MetricsListDataFrames_8Feb2024.R) February 8, 2024
+  
